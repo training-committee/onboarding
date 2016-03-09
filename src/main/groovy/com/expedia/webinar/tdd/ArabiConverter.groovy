aa02e2c269207197f3e2f40481ecf36220f105c9
Federico Cayrol <federico.cayrol@orbitz.com>
2016-03-09 10:07:08 -0300

    More Refactor

--- src/main/groovy/com/expedia/webinar/tdd/ArabiConverter.groovy
+++ src/main/groovy/com/expedia/webinar/tdd/ArabiConverter.groovy
@@ -1,18 +1,22 @@
 package com.expedia.webinar.tdd
 
-
 class ArabiConverter {
     static final Map SIMBOLO = [1: "I", 4: "IV", 5: "V", 9: "IX", 10: 'X', 40: "XL", 50: "L", 90: "XC", 100: "C"]
-    static final List NUMEROS_ESPECIALES = [100, 90, 50, 40, 10, 9, 5, 4, 1]
 
     String toRomano(int numero) {
         StringBuilder out = new StringBuilder()
-        NUMEROS_ESPECIALES.each { int numeroEspecial ->
-            while (numero >= numeroEspecial) {
-                out.append(SIMBOLO[numeroEspecial])
-                numero = numero - numeroEspecial
-            }
-        }
+        numerosOrdenados().each { int it -> numero = convertToRoman(numero, it, out) }
         return out.toString()
+    }
+
+    private static int convertToRoman(int numero, int it, StringBuilder out) {
+        while (numero >= it && ((numero -= it) >= 0)) {
+            out.append(SIMBOLO[it])
+        }
+        return numero
+    }
+
+    static List numerosOrdenados() {
+        return SIMBOLO.keySet().toSorted().reverse()
     }
 }
