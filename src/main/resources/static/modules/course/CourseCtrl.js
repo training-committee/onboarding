1369195464f39933d8fae40403bd1d4ce731cf53
Francisco Millone <v-fmillone@expedia.com>
2016-07-12 17:01:22 -0300

    added ui for courses show

---
+++ src/main/resources/static/modules/course/CourseCtrl.js
@@ -0,0 +1,14 @@
+/**
+ * @ngdoc controller
+ * @name app:CourseCtrl
+ *
+ * @description
+ *
+ *
+ * @requires $scope
+ * */
+angular.module('app')
+    .controller('CourseCtrl', ['$scope', '$routeParams', function($scope, $routeParams){
+
+
+}]);
