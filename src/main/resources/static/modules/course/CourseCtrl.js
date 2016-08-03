061026cd4ee2f15ee71f69b06b5662cfcdfbd12d
nusuarez <v-nusuarez@expedia.com>
2016-08-03 11:03:47 -0300

    finishing ui for show

--- src/main/resources/static/modules/course/CourseCtrl.js
+++ src/main/resources/static/modules/course/CourseCtrl.js
@@ -8,8 +8,14 @@
  * @requires $scope
  * */
 angular.module('app')
-    .controller('CourseCtrl', ['$scope', '$routeParams', 'Course', function($scope, $routeParams, Course){
+    .controller('CourseCtrl', ['$scope', '$routeParams', 'Course', '$sce', function($scope, $routeParams, Course, $sce){
 
     	$scope.course = Course.get({id:1});
+
+
+
+    	 $scope.trustSrc = function(src) {
+    		return $sce.trustAsResourceUrl(src);
+  		};
 
 }]);
