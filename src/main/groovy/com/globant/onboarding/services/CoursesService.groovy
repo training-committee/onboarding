02d1e97604934ed94822f0c7cb28fca53ff71aac
nusuarez <v-nusuarez@expedia.com>
2016-08-03 10:47:54 -0300

    added image to agile course

--- src/main/groovy/com/globant/onboarding/services/CoursesService.groovy
+++ src/main/groovy/com/globant/onboarding/services/CoursesService.groovy
@@ -10,12 +10,12 @@ class CoursesService {
     def getCourses() {
         log.debug("Entering getCourses...")
         //TODO: logic to find the curse corresponding to an user
-        def res = [new Course(id: 1, title:"Angul1", videoUrl:"https://github.com/training-committee/onboarding", name: "AngularJS", description: "AngularJS Course", imageUrl: node_modules/bower/lib/node_modules/hawk/images/hawk.png),
+        def res = [new Course(id: 1, title:"Angul1", videoUrl:"https://github.com/training-committee/onboarding", name: "AngularJS", description: "AngularJS Course", imageUrl: 'node_modules/bower/lib/node_modules/hawk/images/hawk.png'),
                    new Course(id: 2, title:"Angul1", videoUrl:"https://github.com/training-committee/onboarding", name: "HTML5", description: "HTML5 Course", imageUrl: node_modules/bower/lib/node_modules/hawk/images/hawk.png),
                    new Course(id: 3, title:"Angul1", videoUrl:"https://github.com/training-committee/onboarding", name: "CSS3", description: "CSS3 Course", imageUrl: node_modules/bower/lib/node_modules/hawk/images/hawk.png)]
     }
 
     Course findById(Long id){
-        return new Course(id: id, title:'Angul1', videoUrl:'https://github.com/training-committee/onboarding', name: 'AngularJS', description: 'Angular Course', imageUrl: node_modules/bower/lib/node_modules/hawk/images/hawk.png)
+        return new Course(id: id,  title:'Trabajo Colaborativo y Comuncación', videoUrl:'https://www.youtube.com/embed/EYxDctvbaUQ', description: 'III jornadas nacionales de metodologias agiles', imageUrl: '/img/AA2016.png')
     }
 }
